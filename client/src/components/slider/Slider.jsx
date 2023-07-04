5483df8d223678cf506572fad42eef02ab7df3cc
wersomd <ileskazybek@gmail.com>
2023-07-04 21:27:52 +0600

    start slider

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,5 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
+
+const slide_img = [
+    "https://swiperjs.com/demos/images/nature-1.jpg",
+    "https://swiperjs.com/demos/images/nature-2.jpg",
+    "https://swiperjs.com/demos/images/nature-3.jpg",
+    "https://swiperjs.com/demos/images/nature-4.jpg",
+    "https://swiperjs.com/demos/images/nature-5.jpg",
+    "https://swiperjs.com/demos/images/nature-6.jpg",
+    "https://swiperjs.com/demos/images/nature-7.jpg",
+    "https://swiperjs.com/demos/images/nature-8.jpg",
+    "https://swiperjs.com/demos/images/nature-9.jpg",
+];
+
 const Slider = () => {
- return <div>Slider</div>
+    return <section>
+        <Swiper grabCursor={true} centeredSlides={true} slidesPerView={'auto'} className='salesSlide'>
+            {
+                slide_img.map((img, i) => {
+                    return (
+                        <SwiperSlide key={i}>
+                            <img src={img} alt='' />
+                        </SwiperSlide>
+                    )
+                })
+            }
+        </Swiper>
+    </section>
 }
 
 export default Slider
