7b8eaefb999af674b1454433789c5b89cfcbfb59
wersomd <ileskazybek@gmail.com>
2023-07-05 23:01:32 +0600

    add http file

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from 'store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "store";
 
-import App from 'components/app/App'
+import App from "components/app/App";
 
-import './assets/styles/style.scss'
+import "./assets/styles/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+console.log(process.env.REACT_APP_API_URL);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
   </Provider>
-)
+);
